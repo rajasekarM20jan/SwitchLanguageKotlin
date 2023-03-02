58ef72a4fab6e37ed530db1bd327a99d0fb2f02a
Rajasekar <rs20jan1998s@gmail.com>
2023-03-02 19:03:49 +0530

    init

--- app/src/main/java/com/example/switchlanguagekotlin/NewRecipePage.kt
+++ app/src/main/java/com/example/switchlanguagekotlin/NewRecipePage.kt
@@ -1,19 +1,33 @@
 package com.example.switchlanguagekotlin
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import com.firebase.ui.auth.ui.ProgressView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class NewRecipePage : AppCompatActivity() {
     lateinit var backButton:Button
-    lateinit var saveButton:Button
-    lateinit var recipeName:EditText
+    lateinit var saveButton:TextView
+    private lateinit var recipeName:EditText
     lateinit var ingredientsList:EditText
     lateinit var procedure:EditText
+    private lateinit var prepTime:EditText
+    lateinit var dietary:RadioGroup
+    private lateinit var selectedText:String
+    lateinit var p:ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_recipe_page)
@@ -23,6 +37,21 @@ class NewRecipePage : AppCompatActivity() {
         recipeName=findViewById(R.id.recipeName)
         ingredientsList=findViewById(R.id.ingredientsList)
         procedure=findViewById(R.id.procedure)
+        prepTime=findViewById(R.id.prepTime)
+        dietary=findViewById(R.id.dietaryGroup)
+        val db=Firebase.firestore
+        selectedText=""
+
+        try{
+            val selectedRadioButtonId=dietary.checkedRadioButtonId
+
+            val selectedRadioButton=findViewById<RadioButton>(selectedRadioButtonId)
+
+            selectedText=selectedRadioButton.text.toString()
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
+
 
         onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true){
             override fun handleOnBackPressed() {
@@ -32,6 +61,42 @@ class NewRecipePage : AppCompatActivity() {
 
         backButton.setOnClickListener(View.OnClickListener {
             finish()
+        })
+
+
+        saveButton.setOnClickListener(View.OnClickListener {
+
+            if(recipeName.text.length>=4 && ingredientsList.text.length>=20 && procedure.text.length>=50 && prepTime.text.isNotEmpty()){
+                if(selectedText != ""||selectedText.isNotEmpty()){
+                    p=ProgressDialog(this)
+                    p.setMessage("Loading")
+                    p.show()
+                    var recipeTitle:String=recipeName.text.toString()
+                    var recipeIngredients:String=ingredientsList.text.toString()
+                    var recipeProcedure:String=procedure.text.toString()
+                    var preparationTime:String=prepTime.text.toString()+" minutes"
+
+                    var recipe= hashMapOf(
+                        "Ingredients" to recipeIngredients,
+                        "Procedure" to recipeProcedure,
+                        "Preparation time" to preparationTime,
+                        "Dietary" to selectedText
+                    )
+
+                    db.collection("RecipeBook").document(recipeTitle).set(recipe)
+                        .addOnSuccessListener {
+                            if(p.isShowing){
+                                p.dismiss()
+                            }
+                            Toast.makeText(this,"Thank You for sharing your recipe with us.",Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+                }else{
+                    Toast.makeText(this,"Oops! You missed the dietary",Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this,"Aww! Seems you missed some thing for the bucket",Toast.LENGTH_SHORT).show()
+            }
         })
     }
 }
